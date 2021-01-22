3d6727b9cf38790d5b59ef582834861f2fa8857c
Akash Lal <44520393+akashashu1799@users.noreply.github.com>
2021-01-22 19:44:14 +0530

    Delete schema.sql

--- schema.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO supplier (Supplier_No,Supplier_Name,Supplier_Contact_No,Company_Name,Supplier_Address,Company_Contact_No)
-values(5,"Suresh",9685857425,"Ramesh Chemicals","Hsr",1585552500);
-
-
-
-
