40e6016da483619891f49be55dc2a1915f974219
Akash Lal <44520393+akashashu1799@users.noreply.github.com>
2021-01-22 19:40:17 +0530

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,6 @@
+INSERT INTO supplier (Supplier_No,Supplier_Name,Supplier_Contact_No,Company_Name,Supplier_Address,Company_Contact_No)
+values(5,"Suresh",9685857425,"Ramesh Chemicals","Hsr",1585552500);
+
+
+
+
